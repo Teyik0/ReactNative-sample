7ce17c665db2176f92390f20544293201abcc635
teyik0 <teyik0.fr@icloud.com>
2023-06-10 12:00:14 +0200

    Initialisation du projet

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={tw`text-2xl`}>
+        Open up App.js to start working on your app!
+      </Text>
+      <StatusBar style='auto' />
     </View>
   );
 }
